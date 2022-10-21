51b8d9f1713ee81c4f2a70d00fab55e314817c8b
TIAGO PASSOS TEIXEIRA <114706987+passosteixeira@users.noreply.github.com>
2022-10-20 21:03:37 -0300

    Update calculo.js

--- combustivel/calculo.js
+++ combustivel/calculo.js
@@ -4,12 +4,12 @@ function melhor(){
     etanol= parseFloat(frdados.txetanol.value.replace(",","."));
     gasolina= parseFloat(frdados.txgasolina.value.replace(",","."));
     if(etanol < 0.7*gasolina){
-        document.getElementById("imagem").src="imagens/etanol.jpg";
+        document.getElementById("imagem").src="Imagens/etanol.jpg";
     }
     else{
-        document.getElementById("imagem").src="imagens/gasolina.jpg";
+        document.getElementById("imagem").src="Imagens/gasolina.jpg";
     }
 }
 function limpar(){
-    document.getElementById("imagem").src="imagens/normal.jpg";
+    document.getElementById("imagem").src="Imagens/normal.jpg";
 }
